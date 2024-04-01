af2fcd14f9823803d4b44d4171d8bd75926d1f3a
RusdyZain <ryanfajri28@gmail.com>
2024-04-01 16:45:42 +0800

    Done

---
+++ main.js
@@ -0,0 +1,11 @@
+(function ($) {
+  // Skills
+  $(".skill").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
+})(jQuery);
